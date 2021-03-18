208ca6eb682b43aea50029d16e68f5d186df79e4
GIRARDIN-VINCENT Bastien <bastien52300@hotmail.fr>
2021-03-18 11:46:16 +0100

    project + loader

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 'use strict'
 
+function showContent(){
+    document.querySelector('.loader-container').classList.add('hidden');
+}
+
+setTimeout(showContent, 1000);
+
 const ratio = .1;
 const options = {
     root: null,
@@ -61,5 +67,79 @@ $(".driving-head").on('click', (e) => {
     console.log(e.target)
     $(".driving-content").slideToggle(400);
 });
+
+
+
+var timer = 3000;
+
+var i = 0;
+let i2 = 0;
+let i3 = 0;
+let i4 = 0;
+
+var max = $('.carousel img.coop').length;
+let max2 = $('.carousel img.test').length;
+let max3 = $('.carousel img.covid').length;
+let max4 = $('.carousel img.velo').length;
+ 
+$(".carousel img.coop").eq(i).addClass('active').css('left','0');
+$(".carousel img.test").eq(i).addClass('active').css('left','0');
+$(".carousel img.covid").eq(i).addClass('active').css('left','0');
+$(".carousel img.velo").eq(i).addClass('active').css('left','0');
+
+console.log($(".carousel img.test"));
+
+setInterval(function(){ 
+
+    $(".carousel img.coop").removeClass('active');
+    $(".coop").eq(i).css('transition-delay','0.25s');
+
+    $(".carousel img.test").removeClass('active');
+    $(".carousel img.test").eq(i).css('transition-delay','0.25s');
+
+    $(".carousel img.covid").removeClass('active');
+    $(".carousel img.covid").eq(i).css('transition-delay','0.25s');
+
+    $(".carousel img.velo").removeClass('active');
+    $(".carousel img.velo").eq(i).css('transition-delay','0.25s');
+
+    if (i < max-1) {
+        i = i+1; 
+    }
+    else { 
+        i = 0; 
+    }
+
+    if (i2 < max2-1) {
+        i2 = i2+1; 
+    }
+    else { 
+        i2 = 0; 
+    }  
+
+    if (i3 < max3-1) {
+        i3 = i3+1; 
+    }
+    else { 
+        i3 = 0; 
+    }  
+
+    if (i4 < max4-1) {
+        i4 = i4+1; 
+    }
+    else { 
+        i4 = 0; 
+    } 
+
+
+    $(".carousel img.coop").eq(i).css('left','0').addClass('active').css('transition-delay','1.25s');
+    $(".carousel img.test").eq(i).css('left','0').addClass('active').css('transition-delay','1.25s');
+    $(".carousel img.covid").eq(i).css('left','0').addClass('active').css('transition-delay','1.25s');
+    $(".carousel img.velo").eq(i).css('left','0').addClass('active').css('transition-delay','1.25s');
+
+}, timer);
+
+
+ 
 
 
